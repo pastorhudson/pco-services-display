524b3a3edd542c6583346e55c243ce2f6c501ffa
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2021-07-14 16:50:45 -0400

    Added comments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ pco = pypco.PCO(application_id=os.getenv('PCO_APPLICATION_ID'),
 
 
 def get_service_type_id(service_name):
+    """Takes a service type(string) and returns a service type id"""
     services = pco.iterate('/services/v2/service_types')
     for service in services:
         if service['data']['attributes']['name'] == service_name:
@@ -21,21 +22,13 @@ def get_service_type_id(service_name):
 
 
 def get_latest_plan(service_type_id):
+    """Takes a service type id and returns the next Sunday or today's plan if today is a Sunday"""
     plans = pco.iterate(
         f'/services/v2/service_types/{service_type_id}/plans?include=plan_times&order=sort_date&filter=future')
     for plan in plans:
         if plan['data']['attributes']['dates'] == (datetime.strftime(get_sunday(), '%B %d, %Y')):
             # print(plan['data']['attributes']['dates'])
             return plan['data']['id']
-
-
-def get_url():
-    service_type_id = get_service_type_id(os.getenv('SERVICE_TYPE'))
-    plan_id = get_latest_plan(service_type_id)
-    report_template_id = os.getenv('REPORT_TEMPLATE_ID')
-    url = f"https://services.planningcenteronline.com/report_templates/{report_template_id}/report.html?plan_id={plan_id}"
-    print(url)
-    open_url(url)
 
 
 def get_sunday():
@@ -48,9 +41,22 @@ def get_sunday():
         return today
 
 
+def get_url():
+    """This brings everything together and returns the url."""
+    service_type_id = get_service_type_id(os.getenv('SERVICE_TYPE'))
+    plan_id = get_latest_plan(service_type_id)
+    report_template_id = os.getenv('REPORT_TEMPLATE_ID')
+    url = f"https://services.planningcenteronline.com/report_templates/{report_template_id}/report.html?plan_id={plan_id}"
+    return url
+
+
 def open_url(url):
+    """Open's the default browser to the report url. This could be exteded to use solenium or a system command
+    to make the browser go into fullscreen or kiosk mode."""
     webbrowser.open(url)
 
 
 if __name__ == "__main__":
-    get_url()
+    url = get_url()
+    print(url)
+    open_url(url)
